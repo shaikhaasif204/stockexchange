5634473e0c07efb7b93edd87437f5d32490d7a36
sagar7gaikwad <sagar7gaikwad@gmail.com>
2019-07-29 15:57:20 +0530

    initial commit

--- src/main/java/com/hcl/stockex/repository/StockTransactionRepository.java
+++ src/main/java/com/hcl/stockex/repository/StockTransactionRepository.java
@@ -1,5 +1,7 @@
 package com.hcl.stockex.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.hcl.stockex.entity.StockTransaction;
 
 @Repository
 public interface StockTransactionRepository extends JpaRepository<StockTransaction, Long>{
+
+	List<StockTransaction> findAllByUserId(Long userId);
 
 }
--- src/main/java/com/hcl/stockex/serviceimpl/PurchaseServiceImpl.java
+++ src/main/java/com/hcl/stockex/serviceimpl/PurchaseServiceImpl.java
@@ -2,7 +2,9 @@ package com.hcl.stockex.serviceimpl;
 
 import org.springframework.stereotype.Service;
 
+import com.hcl.stockex.service.PurchaseService;
+
 @Service
-public class PurchaseServiceImpl {
+public class PurchaseServiceImpl implements PurchaseService{
 
 }
