497eb6f27c81a80c8e93107de28881e595fe1c40
shivanjali.kanase <kanaseshivanjali@gmail.com>
2019-07-29 17:41:09 +0530

    added dto.

---
+++ src/test/java/com/hcl/stockex/serviceimpl/StockServiceImplTest.java
@@ -0,0 +1,48 @@
+package com.hcl.stockex.serviceimpl;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.hcl.stockex.dto.ResponseDTO;
+import com.hcl.stockex.entity.Stock;
+import com.hcl.stockex.exception.ApplicationException;
+import com.hcl.stockex.repository.StockRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+public class StockServiceImplTest {
+	
+	@InjectMocks
+	StockServiceImpl stockServiceImpl;
+
+	@Mock
+	StockRepository stockRepositoryMock;
+	
+	ResponseDTO responseDTO;
+	List<Stock> stockList = new ArrayList<>();
+
+	@Before
+	public void setUp() {
+		responseDTO = new ResponseDTO();
+		
+	}
+	
+	@Test
+	public void testgetAllStockDetails() throws ApplicationException {
+		
+		Mockito.when(stockRepositoryMock.findAll()).thenReturn(stockList);
+		assertNotNull(stockServiceImpl.getAllStockDetails());
+		
+	}
+	
+
+}
