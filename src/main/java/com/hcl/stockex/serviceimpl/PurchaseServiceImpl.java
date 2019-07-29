004ccb7c300da277c6b79dfbec3904decfec044f
sontrive <sontrive0310@gmail.com>
2019-07-29 16:56:54 +0530

    commit

--- src/main/java/com/hcl/stockex/serviceimpl/PurchaseServiceImpl.java
+++ src/main/java/com/hcl/stockex/serviceimpl/PurchaseServiceImpl.java
@@ -2,6 +2,7 @@ package com.hcl.stockex.serviceimpl;
 
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -9,9 +10,13 @@ import org.springframework.stereotype.Service;
 import com.hcl.stockex.dto.PurchaseRequestDTO;
 import com.hcl.stockex.dto.PurchaseResponseDTO;
 import com.hcl.stockex.dto.ResponseDTO;
+import com.hcl.stockex.entity.Stock;
 import com.hcl.stockex.entity.StockTransaction;
+import com.hcl.stockex.entity.User;
 import com.hcl.stockex.exception.ApplicationException;
+import com.hcl.stockex.repository.StockRepository;
 import com.hcl.stockex.repository.StockTransactionRepository;
+import com.hcl.stockex.repository.UserRepository;
 import com.hcl.stockex.service.PurchaseService;
 import com.hcl.stockex.util.RequestStatusUtil;
 
@@ -20,6 +25,12 @@ public class PurchaseServiceImpl implements PurchaseService {
 
 	@Autowired
 	StockTransactionRepository stockTransactionRepository;
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	StockRepository stockRepository;
 
 
 	@Override
@@ -41,10 +52,59 @@ public class PurchaseServiceImpl implements PurchaseService {
 		responseDTO.setHttpStatus(HttpStatus.NOT_FOUND);
 		return responseDTO;
 	}
+	
+	
 	@Override
 	public ResponseDTO purchaseStock(PurchaseRequestDTO purchaseRequestDTO) throws ApplicationException {
 		
-		return null;
+		ResponseDTO responseDTO = new ResponseDTO();
+		PurchaseResponseDTO responseObject = new PurchaseResponseDTO();
+		
+		StockTransaction transaction = new StockTransaction();
+		
+		Optional<User> optionalUserId = userRepository.findById(purchaseRequestDTO.getUserId());
+		
+		User user;
+		if(optionalUserId.isPresent()) {
+			user = optionalUserId.get();
+		}else {
+			throw new ApplicationException("Invalid user id.");
+		}
+		
+		Optional<Stock> optionalStockId = stockRepository.findById(purchaseRequestDTO.getStockId());
+		Stock stock;
+		
+		if(optionalStockId.isPresent()) {
+			stock = optionalStockId.get();
+		}else {
+			throw new ApplicationException("Invalid stock id.");
+		}
+		
+		transaction.setQuantity(purchaseRequestDTO.getQuantityOfStock());
+		transaction.setStatus(RequestStatusUtil.INITIATED);
+		transaction.setStockPrice(stock.getPurchasePrice());
+		transaction.setTotalPrice(purchaseRequestDTO.getQuantityOfStock()*stock.getPurchasePrice());
+		transaction.setStock(stock);
+		transaction.setUser(user);
+		
+	    StockTransaction savedTransaction = stockTransactionRepository.save(transaction);
+	    
+	    BeanUtils.copyProperties(savedTransaction, responseObject);
+	    responseObject.setUserId(user.getId());
+	    responseObject.setPerStockPrice(stock.getPurchasePrice());
+	    responseObject.setQuantityOfStock(purchaseRequestDTO.getQuantityOfStock());
+	    responseObject.setStatus(RequestStatusUtil.INITIATED);
+	    responseObject.setStockId(stock.getId());
+	    responseObject.setStockName(stock.getStockName());
+	    responseObject.setStockType(stock.getStockType());
+	    responseObject.setTotalprice(purchaseRequestDTO.getQuantityOfStock()*stock.getPurchasePrice());
+
+		
+		responseDTO.setHttpStatus(HttpStatus.OK);
+		responseDTO.setMessage("The stock prices change regularly and the quote is subject to change.  The order will be placed with the live price at the time of submission. "+"   "+"Order has been placed successfully");
+		responseDTO.setData(responseObject);
+		
+		return responseDTO;
 	}
 
 
