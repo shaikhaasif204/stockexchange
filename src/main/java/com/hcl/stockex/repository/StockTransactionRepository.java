71acbe19f788b3083ddb91fec1354e5048bcc33d
Rajat <bigdatarajatpancholi@gmail.com>
2019-07-29 14:44:51 +0530

    Repositories committed

--- src/main/java/com/hcl/stockex/repository/StockTransactionRepository.java
+++ src/main/java/com/hcl/stockex/repository/StockTransactionRepository.java
@@ -1,9 +1,11 @@
 package com.hcl.stockex.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.hcl.stockex.entity.StockTransaction;
 
+@Repository
 public interface StockTransactionRepository extends JpaRepository<StockTransaction, Long>{
 
 }
