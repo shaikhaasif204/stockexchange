f974553289dabf67d08b07fc401d7d4d4bfb42f7
pracheerjain <pracheer2002@gmail.com>
2019-07-29 14:36:57 +0530

    junit

---
+++ src/test/java/com/hcl/stockex/controller/PurchaseControllerTest.java
@@ -0,0 +1,12 @@
+package com.hcl.stockex.controller;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PurchaseControllerTest {
+
+	@InjectMocks
+	PurchaseController purchaseController;
+}
---
+++ src/test/java/com/hcl/stockex/serviceimpl/PurchaseServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.hcl.stockex.serviceimpl;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PurchaseServiceImplTest {
+
+	@InjectMocks
+	PurchaseServiceImpl purchaseServiceImpl;
+}
