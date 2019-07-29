3a625a859f909d8968977b280a82b7763045d454
sontrive <sontrive0310@gmail.com>
2019-07-29 14:29:00 +0530

    change

--- src/main/java/com/hcl/stockex/repository/UserRepository.java
+++ src/main/java/com/hcl/stockex/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.hcl.stockex.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.hcl.stockex.entity.User;
+
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, Long>  {
 
 }
