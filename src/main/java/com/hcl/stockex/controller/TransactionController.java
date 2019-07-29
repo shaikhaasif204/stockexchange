712d82771d12b837f8b5e7cc4b544e6a4b5ade3c
sagar7gaikwad <sagar7gaikwad@gmail.com>
2019-07-29 16:37:51 +0530

    initial commit

--- src/main/java/com/hcl/stockex/controller/TransactionController.java
+++ src/main/java/com/hcl/stockex/controller/TransactionController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hcl.stockex.dto.ResponseDTO;
+import com.hcl.stockex.exception.ApplicationException;
+import com.hcl.stockex.repository.UserRepository;
 import com.hcl.stockex.serviceimpl.TransactionServiceImpl;
 
 @RestController
@@ -18,8 +20,12 @@ public class TransactionController {
 	@Autowired
 	TransactionServiceImpl transactionServiceImpl;
 	
+	
+	
 	@GetMapping(value = "/history")
-	public ResponseEntity<ResponseDTO> getHistory(@RequestParam Long userId) {
+	public ResponseEntity<ResponseDTO> getHistory(@RequestParam Long userId) throws ApplicationException {
+		if(null == userId)
+		throw new ApplicationException("user Id cannot be empty");
 		ResponseDTO returnedResponse = transactionServiceImpl.getHistory(userId);
 		return new ResponseEntity<ResponseDTO>(returnedResponse, HttpStatus.OK);
 		
--- src/main/java/com/hcl/stockex/serviceimpl/TransactionServiceImpl.java
+++ src/main/java/com/hcl/stockex/serviceimpl/TransactionServiceImpl.java
@@ -11,6 +11,7 @@ import com.hcl.stockex.dto.PurchaseResponseDTO;
 import com.hcl.stockex.dto.ResponseDTO;
 import com.hcl.stockex.entity.StockTransaction;
 import com.hcl.stockex.repository.StockTransactionRepository;
+import com.hcl.stockex.repository.UserRepository;
 import com.hcl.stockex.service.TransactionService;
 
 @Service
@@ -19,20 +20,29 @@ public class TransactionServiceImpl implements TransactionService{
 	@Autowired
 	StockTransactionRepository stockTransactionRepository;
 	
+	@Autowired
+	UserRepository userRepository;
 	
 	@Override
 	public ResponseDTO getHistory(Long userId) {
-		List<StockTransaction> history = stockTransactionRepository.findAllByUserId(userId);
-		System.out.println(history);
-		
 		ResponseDTO responseDTO = new ResponseDTO();
-		List<PurchaseResponseDTO> purchaseResponseDTOList = history.stream().map(i -> new PurchaseResponseDTO(i.getUser().getId(),i.getStock().getId(),
-				i.getStock().getStockName(),i.getStock().getStockType(),i.getTotalPrice(),i.getStockPrice(),i.getQuantity())).collect(Collectors.toList());
-		responseDTO.setHttpStatus(HttpStatus.OK);
-		responseDTO.setMessage("List of Past Purchase Tradings");
-		responseDTO.setData(purchaseResponseDTOList);
 		
+		if((userRepository.findById(userId).isPresent())) {
+			List<StockTransaction> history = stockTransactionRepository.findAllByUserId(userId);
+			
+			List<PurchaseResponseDTO> purchaseResponseDTOList = history.stream().map(i -> new PurchaseResponseDTO(i.getUser().getId(),i.getStock().getId(),
+					i.getStock().getStockName(),i.getStock().getStockType(),i.getTotalPrice(),i.getStockPrice(),i.getQuantity())).collect(Collectors.toList());
+			responseDTO.setHttpStatus(HttpStatus.OK);
+			responseDTO.setMessage("List of Past Purchase Tradings");
+			responseDTO.setData(purchaseResponseDTOList);
+			
+			return responseDTO;
+		}
+		responseDTO.setHttpStatus(HttpStatus.BAD_REQUEST);
+		responseDTO.setMessage("User does not exist");
+		responseDTO.setData(null);
 		return responseDTO;
+		
 	}
 
 	
