5bf721769ee92e48583aaa380135e322d396af99
sontrive <sontrive0310@gmail.com>
2019-07-29 14:12:02 +0530

    commit

---
+++ src/main/java/com/hcl/stockex/util/RequestStatusUtil.java
@@ -0,0 +1,10 @@
+package com.hcl.stockex.util;
+
+public class RequestStatusUtil {
+
+	public static final Integer INITIATED = 0;
+	public static final Integer CONFIRMED = 1;
+	public static final Integer EXECUTED = 3;
+	public static final Integer DECLINED = 4;
+	
+}
