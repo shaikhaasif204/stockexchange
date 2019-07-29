94dfb9d6179226db09bd3a1a51e320997410666a
sontrive <sontrive0310@gmail.com>
2019-07-29 15:11:28 +0530

    commit

--- src/main/java/com/hcl/stockex/serviceimpl/PurchaseServiceImpl.java
+++ src/main/java/com/hcl/stockex/serviceimpl/PurchaseServiceImpl.java
@@ -2,7 +2,18 @@ package com.hcl.stockex.serviceimpl;
 
 import org.springframework.stereotype.Service;
 
+import com.hcl.stockex.dto.PurchaseRequestDTO;
+import com.hcl.stockex.dto.ResponseDTO;
+import com.hcl.stockex.exception.ApplicationException;
+import com.hcl.stockex.service.PurchaseService;
+
 @Service
-public class PurchaseServiceImpl {
+public class PurchaseServiceImpl implements PurchaseService{
+
+	@Override
+	public ResponseDTO purchaseStock(PurchaseRequestDTO purchaseRequestDTO) throws ApplicationException {
+		
+		return null;
+	}
 
 }
